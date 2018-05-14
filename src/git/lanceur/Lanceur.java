035ff79a8097c39eb97b92ee8c50c40371b0bd33
KindoTech <dialloabdourahmane68@gmail.com>
2018-05-14 19:59:15 +0000

    Ajout Branche et quelque classe

--- src/git/lanceur/Lanceur.java
+++ src/git/lanceur/Lanceur.java
@@ -4,5 +4,6 @@ public class Lanceur {
 	
 	public static void main(String[] args) {
 		System.out.println("Mon premier projet git");
+		System.out.println("Demo sur les branches");
 	}
 }
---
+++ src/git/lanceur/MyBranch.java
@@ -0,0 +1,10 @@
+package git.lanceur;
+
+public class MyBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Branche");
+	}
+
+}
