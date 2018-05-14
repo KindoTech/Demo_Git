d4a9358f46c3fb8c720879bf4eaffe2186cad8e5
KindoTech <dialloabdourahmane68@gmail.com>
2018-05-14 19:48:15 +0000

    Creation de l'interface et maClasse

---
+++ src/git/inter/MonInterface.java
@@ -0,0 +1,5 @@
+package git.inter;
+
+public interface MonInterface {
+	public void faireLaSomme(int x, int y);
+}
---
+++ src/git/lanceur/MaClasse.java
@@ -0,0 +1,13 @@
+package git.lanceur;
+
+import git.inter.MonInterface;
+
+public class MaClasse implements MonInterface {
+
+	@Override
+	public void faireLaSomme(int x, int y) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
