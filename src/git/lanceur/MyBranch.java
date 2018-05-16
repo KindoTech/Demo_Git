12410323e8f3278539dc958442c227418067536e
KindoTech <dialloabdourahmane68@gmail.com>
2018-05-16 19:22:57 +0000

    Modification de lanceur

--- src/git/lanceur/MyBranch.java
+++ src/git/lanceur/MyBranch.java
@@ -5,6 +5,7 @@ public class MyBranch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My Branche");
+		System.out.println("en ligne");
 	}
 
 }
